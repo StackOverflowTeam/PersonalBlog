ac1a87f39f2deecd0846d82388e4a40ce64bea3a
skyrocketingHong <null@skyrocketing.ninja>
2020-07-05 10:40:23 +0800

    用户相关接口使用RespBean返回结果

--- src/main/java/team/stackoverflow/personalsite/controller/AdminUserController.java
+++ src/main/java/team/stackoverflow/personalsite/controller/AdminUserController.java
@@ -6,6 +6,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import team.stackoverflow.personalsite.pojo.AdminUser;
 import team.stackoverflow.personalsite.service.AdminUserService;
+import team.stackoverflow.personalsite.util.RespBean;
 
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
@@ -72,14 +73,14 @@ public class AdminUserController {
     
     //带session的登录接口
     @PostMapping(value = "/login")
-    public String login(@RequestParam("userName") String userName,
-                        @RequestParam("password") String password,
-                        @RequestParam("verifyCode") String verifyCode,
-                        HttpSession session) {
+    public RespBean login(@RequestParam("userName") String userName,
+                          @RequestParam("password") String password,
+                          @RequestParam("verifyCode") String verifyCode,
+                          HttpSession session) {
         String captchaCode = session.getAttribute("verifyCode") + "";
         if (StringUtils.isEmpty(captchaCode) || !verifyCode.equals(captchaCode)) {
             session.setAttribute("errorMsg", "验证码错误");
-            return "admin/login";
+            return new RespBean("error", "failure");
         }
         AdminUser adminUser = adminUserService.login(userName, password);
         if (adminUser != null) {
@@ -87,66 +88,60 @@ public class AdminUserController {
             session.setAttribute("loginUserId", adminUser.getAdminUserId());
             //session过期时间设置为7200秒 即两小时
             session.setMaxInactiveInterval(60 * 60 * 2);
-            return "redirect:/#/home";
+            return new RespBean("success", "success");
         } else {
             session.setAttribute("errorMsg", "登陆失败");
-            return "admin/login";
+            return new RespBean("error", "failure");
         }
     }
     
     @GetMapping("/profile")
-    public String profile(HttpServletRequest request) {
+    public RespBean profile(HttpServletRequest request) {
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
         AdminUser adminUser = adminUserService.getUserDetailById(loginUserId);
         if (adminUser == null) {
-            return "admin/login";
+            return new RespBean("error", "failure");
         }
         request.setAttribute("path", "profile");
         request.setAttribute("loginUserName", adminUser.getLoginUserName());
         request.setAttribute("nickName", adminUser.getNickName());
-        return "admin/profile";
+        return new RespBean("success", "success");
     }
     
     //修改密码
     @PostMapping("/profile/password")
-    public String passwordUpdate(HttpServletRequest request,
-                                 @RequestParam("originalPassword") String originalPassword,
-                                 @RequestParam("newPassword") String newPassword) {
-        if (StringUtils.isEmpty(originalPassword) || StringUtils.isEmpty(newPassword)) {
-            return "参数不能为空";
-        }
+    public RespBean passwordUpdate(HttpServletRequest request,
+                                   @RequestParam("originalPassword") String originalPassword,
+                                   @RequestParam("newPassword") String newPassword) {
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
         if (adminUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
             //修改成功后清空session中的数据，前端控制跳转至登录页
             request.getSession().removeAttribute("loginUserId");
             request.getSession().removeAttribute("loginUser");
             request.getSession().removeAttribute("errorMsg");
-            return "success";
+            return new RespBean("success", "success");
         } else {
-            return "修改失败";
+            return new RespBean("error", "failure");
         }
     }
     
     @PostMapping("/profile/name")
-    public String nameUpdate(HttpServletRequest request,
-                             @RequestParam("loginUserName") String loginUserName,
-                             @RequestParam("nickName") String nickName) {
-        if (StringUtils.isEmpty(loginUserName) || StringUtils.isEmpty(nickName)) {
-            return "参数不能为空";
-        }
+    public RespBean nameUpdate(HttpServletRequest request,
+                               @RequestParam("loginUserName") String loginUserName,
+                               @RequestParam("nickName") String nickName) {
         Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
         if (adminUserService.updateName(loginUserId, loginUserName, nickName)) {
-            return "success";
+            return new RespBean("success", "success");
         } else {
-            return "修改失败";
+            return new RespBean("error", "failure");
         }
     }
     
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request) {
+    public RespBean logout(HttpServletRequest request) {
         request.getSession().removeAttribute("loginUserId");
         request.getSession().removeAttribute("loginUser");
         request.getSession().removeAttribute("errorMsg");
-        return "admin/login";
+        return new RespBean("success", "success");
     }
 }
